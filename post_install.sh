223c523999b1658542ed8bfdef6a985e9fd066a5
Sharad Shriram <sharadshriram01@gmail.com>
2025-01-05 15:55:32 +0530

    remove jekyll

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # This post install script works only for debian based, Gnome desktops.
 
-
 ## Download color themes for gnome-terminal
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
 bahs -c "$(curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -)"
@@ -14,7 +13,6 @@ sudo apt dist-upgrade
 ## Installing deb packages
 sudo apt install --upgrade curl git neovim tmux python3-virtualenv virtualbox virtualbox-ext-pack 
 
-
 ## Install brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
@@ -26,21 +24,12 @@ sudo apt install brave-browser
 ## Install steam
 sudo apt install steam
 
-
 ## cappucin theme for tmux
 mkdir -p ~/.config/tmux/plugins/catppuccin
 git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 touch ~/.config/tmux/tmux.conf
 echo "run ~/.config/tmux/plugins/catppuccin/tmux/catppuccin.tmux" >> ~/.config/tmux/tmux.conf
 tmux source ~/.config/tmux/tmux.conf
-
-## Install jekyll
-sudo apt-get install ruby-full build-essential zlib1g-dev
-echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
-echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
-echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-gem install jekyll bundler
 
 ## Cleanup
 sudo apt clean
